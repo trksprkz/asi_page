f80d316017dfa1cf18c4d97a871ef0e50f0b43ec
Issac <issacndavis@gmail.com>
2023-01-02 10:47:07 -0600

    adjusted scrolling

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,6 +3,7 @@ import styles from "../styles/Section.module.css";
 import '../index.css';
 import 'animate.css';
 import { Link } from 'react-router-dom'
+import { Fade } from "react-awesome-reveal";
 
 function Section(props) {
   return (
@@ -10,6 +11,7 @@ function Section(props) {
 
 
     <div style={{ backgroundImage: `url(${props.backgroundImg})`}} className={styles.wrap} >
+      <Fade bottom>
             <div className={styles.overlay}>
 
                 <div className={styles.text2}>
@@ -17,9 +19,11 @@ function Section(props) {
                     <a href={props.link}><p style={{ color: `#${props.pColor};` }} className={styles.p_enter}>{props.tag}</p></a>
                 </div>
             </div>
+      </Fade>
 
 
       <div className={styles.buttonGroup}>
+        <Fade bottom>
         <div className={styles.leftButton}>
         <Link to="/inquiry" className={styles.leftText}>{props.leftBtnText}</Link>
         </div>
@@ -27,9 +31,8 @@ function Section(props) {
         <div className={styles.rightButton}>
             <a href='https://www.com' className={styles.rightText}>{props.rightBtnText}</a>
         </div>
-
+        </Fade>
       </div>
-
       <div>
       <img src={require('../icons/image-svg.svg').default} alt='down arrow' className={styles.downArrow} />
       </div>
